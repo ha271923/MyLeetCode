7f6e85c6596a99c62642244b9329a72e3c74f595
Wei <ha271923@gmail.com>
2025-09-11 15:01:11 +0800

    init

---
+++ app/src/test/java/com/hawk/myleetcode/AddBinary.java
@@ -0,0 +1,67 @@
+package com.hawk.myleetcode;
+
+import org.junit.Test;
+
+/**
+ * Created by Edward on 28/07/2017.
+ */
+public class AddBinary {
+    /**
+     * 67. Add Binary
+     * Given two binary strings, return their sum (also a binary string).
+
+     For example,
+     a = "11"
+     b = "1"
+     Return "100".
+
+     time : O(n);
+     space : O(n);
+     * @param a
+     * @param b
+     * @return
+     */
+    @Test
+    public void main() {
+        System.out.println("----------------------------------------------------------------");
+        // System.out.println(addBinary("11","1"));
+        System.out.println(addBinary("1010","1011"));
+    }
+/*
+   LOOP:   9876543210
+           ----------
+   sBin1         1010
+   sBin2         1011
+           ----------
+                    1
+                  10    => 2 -> sum=a[1]+b[1]+carry, carry=sum/2=1, sb=sum%2=0
+                  0
+                10      => 2 -> sum=a[3]+b[3]+carry, carry=sum/2=1, sb=sum%2=0
+           ----------
+     Ans        10101
+
+ */
+    public static String addBinary(String sBin1, String sBin2) {
+        StringBuilder sb = new StringBuilder();
+        int i1 = sBin1.length() - 1;
+        int i2 = sBin2.length() - 1;
+        int carry = 0; // 餘數
+        while (i1 >= 0 || i2 >= 0) { // LOOP: i1與i2長度可能不相等
+            int sum = carry;
+            if (i1 >= 0) // 此時i2可能已經 < 0
+                sum += sBin1.charAt(i1) - '0';
+            if (i2 >= 0) // 此時i1可能已經 < 0
+                sum += sBin2.charAt(i2) - '0';
+            // KEY: algorithm
+            carry = sum / 2;
+            sb.append(sum % 2);
+
+            System.out.println("i1="+i1+"  i2="+i2+" sb="+sb +"  sum="+sum);
+            i1--;i2--;
+        }
+        if (carry != 0) { // Tips: 最後的餘數
+            sb.append(carry);
+        }
+        return sb.reverse().toString(); // KEY: 輸出前將append後的次序,反轉
+    }
+}
---
+++ app/src/test/java/com/hawk/myleetcode/TwoSum.java
@@ -0,0 +1,76 @@
+package com.hawk.myleetcode;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+
+public class TwoSum {
+    @Test
+    public void main() {
+        int[] nums = new int[]{  2  ,  7  ,  11  ,  15  };
+        int   target = 9;
+        int[] res = twoSum(nums, target);
+
+        System.out.println("----------------------------------------------------------------");
+        System.out.println("res[0]="+res[0]);
+        System.out.println("res[1]="+res[1]);
+    }
+
+    /**
+     *  1. Two Sum
+
+     Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+     You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+     Given nums = [2, 7, 11, 15], target = 9,
+
+     Because nums[0] + nums[1] = 2 + 7 = 9,
+
+     return [0, 1].
+
+     time : O(n)
+     space : O(n)
+     * @param nums
+     * @param target
+     * @return
+     */
+
+    /**
+     *   index =    0     1      2      3    Value
+     *  target =    9     9      9      9
+     *    nums = [  2  ,  7  ,  11  ,  15  ]
+     *    diff =    7     2     -2     -6    Key
+     *
+     *
+     *
+     *   Ans:
+     *    inputs=[  2  ,  7  ,  11  ,  15  ] Value[i]
+     *    --------------------------------------
+     *  diff=9 |    7     2     -2     -6   <- diffs[i]
+     */
+    public static int[] twoSum(int[] nums, int target) {
+
+        if (nums == null || nums.length < 2) {
+            return new int[]{-1, -1};
+        }
+        // ??? twoSum題目沒說input array是否是sorted
+        int[] res = new int[]{-1, -1};
+        HashMap<Integer, Integer> map = new HashMap<>();
+        // KEY: 原本需要兩個LOOPs=LOOP1+LOOP2=RESULT的演算法找出答案
+        for (int i = 0; i < nums.length; i++) { // KEY: 現在利用LOOP1[i]算出答案, LOOP1[i+1]利用hashmap快速查找已記錄的答案
+            // 2. i=i+1
+            // TRUE == map.containsKey(V)  return boolean type, not int.
+            if (map.containsKey(target - nums[i])) { // KEY: 3. SEARCH ALL by contains: 比對快取, 尋找是否有所缺少的差值 , KEY: 在已儲存的資料中查找
+                // V = map.get(K)
+                res[0] = map.get(target - nums[i]);  // 4. LOAD: 該差值的V就是index_1
+                res[1] = i; // 4. LOAD: 當前的index就是index_2
+                break;
+            }
+            // map.put(K,V)
+            map.put(nums[i], i); // 1. KEY: CACHE: 將掃過的數值存入快取, 利用HashMap的 Time-Complexity 優勢
+        }
+
+        return res;
+    }
+}
