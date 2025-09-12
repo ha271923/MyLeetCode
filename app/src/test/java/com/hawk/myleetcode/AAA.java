679c059ea9b88de09fc56e9c2f79c678ec8246da
Wei <ha271923@gmail.com>
2025-09-12 18:49:32 +0800

    A template

---
+++ app/src/test/java/com/hawk/myleetcode/AAA.java
@@ -0,0 +1,16 @@
+package com.hawk.myleetcode;
+
+import org.junit.Test;
+
+public class AAA {
+    @Test
+    public void main() {
+        System.out.println("----------------------------------------------------------------");
+        int out = helper(10);
+        System.out.println("out="+out);
+    }
+    public static int helper(int a) {
+        a = 50;
+        return a;
+    }
+}
